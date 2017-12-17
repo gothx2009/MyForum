c234b2157d62fc842d6b81041f895a68e53d92c3
Matthew Lutz <me@mlutz.us>
2017-12-17 17:17:31 -0600

    Re-format Display class

--- inc/class.display.php
+++ inc/class.display.php
@@ -1,9 +1,14 @@
 <?php
-  class Display {
-    var $to_output;
-    function output() {
-      $html = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><link rel='stylesheet' href='css/default.css'></head>";
-      echo $html;
-    }
-  }
+	class Display {
+		var $to_output;
+		function output() {
+			$html  = "<!DOCTYPE html><html lang='en'><head>";
+			// Meta Data
+			$html .= "<meta charset='utf-8'>";
+			// CSS
+			$html .= "<link rel='stylesheet' href='css/default.css'>";
+			$html .= "</head><body><div id='wrapper'>";
+			echo $html;
+		}
+	}
 ?>
