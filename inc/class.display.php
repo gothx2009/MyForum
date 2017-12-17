67fba0f028ada12c21beb42da6612631943e4039
Matthew Lutz <me@mlutz.us>
2017-12-17 17:01:26 -0600

    Update class.display.php

--- inc/class.display.php
+++ inc/class.display.php
@@ -3,7 +3,7 @@
     var $to_output;
     function output() {
       $html = "<!DOCTYPE html><html lang='en'><head><meta charset='utf-8'><link rel='stylesheet' href='css/default.css'></head>";
-      echo filter_var($html, FILTER_DEFAULT);
+      echo $html;
     }
   }
 ?>
