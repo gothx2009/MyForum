c190d94e73b86b01758e7f09c263a3020975e427
Matthew Lutz <me@mlutz.us>
2017-12-17 23:20:42 -0600

    Post deletion almost complete.

--- source/pin.php
+++ source/pin.php
@@ -1,12 +1,35 @@
 <?php
 	class Pin {
 		function __construct() {
-			global $display;
+			global $display, $board;
 			$display->show_post_form = false;
 			if($_SERVER['REQUEST_METHOD'] == "POST") {
-				
+				$code = isset($_POST['code']) ? intval($_POST['code']) : false;
+				$id = isset($_POST['id']) ? intval($_POST['id']) : false;
+				$pin = isset($_POST['adminpin']) ? $_POST['adminpin'] : false;
+				if(!$pin) {
+					$_SESSION['error'] = array("error", "You must enter a pin.");
+					$this->show_pin_form();
+				} if($pin !== $board['admin_pin']) {
+					$_SESSION['error'] = array("error", "Invalid PIN");
+					$this->show_pin_form();
+				} else {
+					switch($code) {
+						case 1:
+						default:
+							$this->delete_post($id);
+							break;
+					}
+				}
 			} else {
 				$this->show_pin_form();
+			}
+		}
+		function delete_post($id) {
+			global $db;
+			$sql = "DELETE FROM p WHERE i='". $id ."'";
+			if($result = $db->query($sql)) {
+				
 			}
 		}
 		function show_pin_form() {
@@ -14,14 +37,14 @@
 			$display->crumbs[] = "Administration PIN";
 			$code = isset($_GET['c']) ? intval($_GET['c']) : false;
 			$id = isset($_GET['i']) ? intval($_GET['i']) : false;
-			$html = "<div class='category pinform'><form action='./index.php?act=pin'><input type='hidden' name='code' value='". $code ."'><input type='hidden' name='id' value='". $id ."'><div class='maintitle'>Administration PIN</div><table><tr><td>";
+			$html = "<div class='category pinform'><form action='./index.php?act=pin' method='post'><input type='hidden' name='code' value='". $code ."'><input type='hidden' name='id' value='". $id ."'><div class='maintitle'>Administration PIN</div><table><tr><td>";
 			switch($code) {
 				case 1:
 				default:
 					$html .= "Enter the PIN to delete post #". $id;
 					break;
 			}
-			$html .= "<br /><br /><input type='text' name='adminpin'></td></tr></table></form></div>";
+			$html .= "<br /><br /><input type='text' name='adminpin'></td></tr><tr><td><input type='submit' value='Submit'></td></tr></table></form></div>";
 			
 			$display->to_output .= $html;
 		}
