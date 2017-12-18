9da3e4c7295d4b6fc3be253f0c71d5c814b4f993
Matthew Lutz <me@mlutz.us>
2017-12-17 19:34:39 -0600

    Start building SQL

--- inc/class.myforum.php
+++ inc/class.myforum.php
@@ -1,5 +1,6 @@
 <?php
 	class MyForum {
+		var $in_forum = 0;
 		var $sym_version = "0.1.0";
 		var $version = "1";
 	}
--- source/post.php
+++ source/post.php
@@ -1,2 +1,5 @@
 <?php
+	$title = isset($_POST['ttitle']) ? $_POST['ttitle'] : false;
+	$sql = "INSERT INTO t(pinned,title) VALUES (0,'".$title."');";
+	$sql = "INSERT INTO p(i,a,b) VALUES ('','','');";
 ?>
