13bf6a0cfac9c87a7309011f3d5e04a2bc52cb1e
Matthew Lutz <me@mlutz.us>
2017-12-17 23:53:51 -0600

    Finished first upgrade

--- upgrade/1.php
+++ upgrade/1.php
@@ -1,5 +1,10 @@
 <?php
+	include("inc/config.php");
+	$db = new mysqli($sql['hostname'],$sql['username'],$sql['password'],$sql['database']);
 	$sql = array();
 	$sql[] = "ALTER TABLE `t` ADD `aemail` VARCHAR(255) NULL DEFAULT NULL AFTER `pinned`;";
 	$sql[] = "ALTER TABLE `p` ADD `aemail` VARCHAR(255) NOT NULL AFTER `aname`;";
+	foreach($sql as $s) {
+		$db->query($s);
+	}
 ?>
