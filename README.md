02284d1ce2d6f0fcb63f5ae0e873c37b1565acde
Matthew Lutz <me@mlutz.us>
2017-12-17 21:11:19 -0600

    0..5.0 Release

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MyForum
+MyForum
+==========
+Romantic Version: `0.5.0`
+
+Symantic Version: `1-DEV`
+
+### Changelog
+- 0.5.0
+  - Initial release
--- inc/class.myforum.php
+++ inc/class.myforum.php
@@ -1,6 +1,6 @@
 <?php
 	class MyForum {
-		var $sym_version = "0.1.0";
-		var $version = "1";
+		var $sym_version = "0.5.0";
+		var $version = "1-DEV";
 	}
 ?>
