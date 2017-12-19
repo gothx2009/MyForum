337a3071925eb563a9695414357414903d30b4e4
Matthew Lutz <me@mlutz.us>
2017-12-18 18:07:02 -0600

    Remove installation stuff from index.

--- index.php
+++ index.php
@@ -7,13 +7,6 @@
 	$myforum = new MyForum;
 	$display = new Display;
 	$db = new mysqli($sql['hostname'],$sql['username'],$sql['password'],$sql['database']);
-	$db->query("CREATE TABLE IF NOT EXISTS p(i INT AUTO_INCREMENT, a INT, b TEXT, KEY(i))");
-	$db->query("ALTER TABLE `p` ADD PRIMARY KEY(`i`)");
-	$db->query("ALTER TABLE `p` DROP INDEX `i`;");
-	$db->query("ALTER TABLE `p` CHANGE `i` `i` INT(100) NOT NULL AUTO_INCREMENT;");
-	$db->query("ALTER TABLE `p` CHANGE `a` `parent` INT(100) NOT NULL DEFAULT '0';");
-	$db->query("ALTER TABLE `p` CHANGE `b` `content` TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;");
-	$db->query("ALTER TABLE `p` ADD `aname` VARCHAR(255) NOT NULL AFTER `parent`;");
 	$act = isset($_GET['act']) ? $_GET['act'] : false;
 	if(isset($_GET['showtopic'])) {
 		$act = "ST";
